e29c477171306c6ad23651a09044719276d87a32
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2024-08-03 09:19:05 +0200

    implement test for database extension

--- lib/src/extensions/database_operation_extension.dart
+++ lib/src/extensions/database_operation_extension.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:eloquent/eloquent.dart';
 import 'package:vania/src/exception/http_exception.dart';
 
-extension DatabaseHelper on QueryBuilder {
+extension DatabaseOperationExtension on QueryBuilder {
   // Create and return inserted data
   Future<Map<String, dynamic>> create(Map<String, dynamic> data) async {
     final insertId = await insertGetId(data);
--- lib/vania.dart
+++ lib/vania.dart
@@ -40,7 +40,7 @@ export 'src/database/migration.dart';
 export 'src/database/seeder/seeder.dart';
 export 'src/enum/column_index.dart';
 export 'src/extensions/pagination_extension.dart';
-export 'src/extensions/database_helper.dart';
+export 'src/extensions/database_operation_extension.dart';
 export 'package:eloquent/src/query/query_builder.dart';
 export 'package:eloquent/eloquent.dart' show QueryException, Connection;
 
