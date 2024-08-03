a6a963e7295c095246b907d2dcb2b4d5173f7f0c
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2024-08-03 08:52:53 +0200

    develop test and update extension map

--- lib/src/extensions/map_extension.dart
+++ lib/src/extensions/map_extension.dart
@@ -1,28 +1,27 @@
 extension MapExtensions on Map<dynamic, dynamic> {
-  Map<String, dynamic> removeParam(String keys) {
-    dynamic value = this;
+  Map<dynamic, dynamic> removeParam(String keys) {
     List<String> parts = keys.split('.');
-    List<String> k = parts.sublist(0, parts.length - 1);
-
-    Map<String, dynamic> data = value;
-    for (String i in k) {
-      data = data[i];
+    Map<dynamic, dynamic> data = this;
+    for (int i = 0; i < parts.length - 1; i++) {
+      if (data[parts[i]] is Map) {
+        data = data[parts[i]];
+      } else {
+        return this; // Early exit if path is invalid
+      }
     }
     data.remove(parts.last);
-    return value;
+    return this;
   }
 
   dynamic getParam(String keys) {
-    dynamic value = this;
     List<String> parts = keys.split('.');
-    List<String> k = parts.sublist(0, parts.length - 1);
-
-    Map<dynamic, dynamic> data = value;
-    for (String i in k) {
-      if (data[i] is List) {
-        return data[i];
+    Map<dynamic, dynamic> data = this;
+    for (int i = 0; i < parts.length - 1; i++) {
+      if (data[parts[i]] is Map) {
+        data = data[parts[i]];
+      } else {
+        return null; // Return null if the path breaks
       }
-      data = data[i];
     }
     return data[parts.last];
   }
