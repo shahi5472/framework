eb9d6e00cfc7789799cbad2169725fdea2e54937
Reza Taghizadeh <alirezataghizadeh@gmail.com>
2024-08-03 10:39:04 +0200

    add documents to extensions

--- lib/src/extensions/map_extension.dart
+++ lib/src/extensions/map_extension.dart
@@ -1,4 +1,29 @@
 extension MapExtensions on Map<dynamic, dynamic> {
+  /// Removes a parameter from a nested map structure based on a dot-separated key path.
+  ///
+  /// This method allows removing a value from a deeply nested map using a path described by a string of keys separated by dots.
+  ///
+  /// **Parameters:**
+  /// - [keys]: A string representing the path to the value to remove, separated by dots. For example, 'user.profile.name'.
+  ///
+  /// **Returns:**
+  /// The original map with the specified parameter removed if found. If any part of the path is invalid (not a map or non-existent key), the original map is returned unchanged.
+  ///
+  /// **Example Usage:**
+  /// ```dart
+  /// var myMap = {
+  ///   'user': {
+  ///     'profile': {
+  ///       'name': 'John',
+  ///       'age': 30
+  ///     }
+  ///   }
+  /// };
+  /// myMap.removeParam('user.profile.name');
+  /// print(myMap); // Outputs: {user: {profile: {age: 30}}}
+  /// myMap.removeParam('user.profile.nonExistentKey'); // Does nothing if key does not exist
+  /// print(myMap); // Outputs: {user: {profile: {age: 30}}}
+  /// ```
   Map<dynamic, dynamic> removeParam(String keys) {
     List<String> parts = keys.split('.');
     Map<dynamic, dynamic> data = this;
@@ -13,6 +38,31 @@ extension MapExtensions on Map<dynamic, dynamic> {
     return this;
   }
 
+  /// Retrieves a parameter from a nested map structure based on a dot-separated key path.
+  ///
+  /// This method facilitates accessing values in a deeply nested map using a path described by a string of keys separated by dots.
+  ///
+  /// **Parameters:**
+  /// - [keys]: A string representing the path to the value, separated by dots. For example, 'user.profile.age'.
+  ///
+  /// **Returns:**
+  /// The value at the specified path if it exists and the path is valid. If the path is broken at any point (not a map or key does not exist), returns `null`.
+  ///
+  /// **Example Usage:**
+  /// ```dart
+  /// var myMap = {
+  ///   'user': {
+  ///     'profile': {
+  ///       'name': 'John',
+  ///       'age': 30
+  ///     }
+  ///   }
+  /// };
+  /// var name = myMap.getParam('user.profile.name');
+  /// print(name); // Outputs: John
+  /// var salary = myMap.getParam('user.profile.salary'); // Path does not exist
+  /// print(salary); // Outputs: null
+  /// ```
   dynamic getParam(String keys) {
     List<String> parts = keys.split('.');
     Map<dynamic, dynamic> data = this;
--- lib/src/extensions/pagination_extension.dart
+++ lib/src/extensions/pagination_extension.dart
@@ -3,6 +3,42 @@ import 'package:vania/src/database/simple_pagination.dart';
 import 'package:vania/vania.dart';
 
 extension Pagination on QueryBuilder {
+  /// Fetches data in a paginated format based on the specified page and number of items per page.
+  ///
+  /// This method constructs a paginated response including navigation links and total counts, which
+  /// makes it easier to implement frontend pagination components.
+  ///
+  /// **Parameters:**
+  /// - [perPage]: Number of items per page. Defaults to 15.
+  /// - [page]: Current page number. Defaults to 1.
+  /// - [basePath]: Base path for generating pagination links. If not provided, it will be taken from the environment.
+  ///
+  /// **Returns:**
+  /// A `Map` containing paginated results and navigation links.
+  ///
+  /// **Example Usage:**
+  /// Suppose you have a `QueryBuilder` configured for a database table with many entries:
+  /// ```dart
+  /// var queryBuilder = QueryBuilder();
+  /// var paginationResult = await queryBuilder.paginate(10, 2, 'http://example.com/api/items');
+  /// print(paginationResult);
+  /// ```
+  /// **Example Output:**
+  /// ```plaintext
+  /// {
+  ///   'total': 200,
+  ///   'perPage': 10,
+  ///   'page': 2,
+  ///   'lastPage': 20,
+  ///   'previousPage': 1,
+  ///   'nextPage': 3,
+  ///   'nextLink': 'http://example.com/api/items?page=3',
+  ///   'previousLink': 'http://example.com/api/items?page=1',
+  ///   'lastLink': 'http://example.com/api/items?page=20',
+  ///   'firstLink': 'http://example.com/api/items?page=1',
+  ///   'data': [...]
+  /// }
+  /// ```
   Future<Map> paginate(
       [int perPage = 15, int page = 1, String? basePath]) async {
     basePath ?? _getValidBaseUrl(env('APP_URL'));
@@ -33,6 +69,33 @@ extension Pagination on QueryBuilder {
     return url;
   }
 
+  /// Fetches data in a simple paginated format without advanced configuration.
+  /// This method is deprecated and will be removed in future releases. Use [simplePaginate] instead.
+  ///
+  /// **Parameters:**
+  /// - [perPage]: Number of items per page, defaults to 15.
+  /// - [page]: Current page number, defaults to 1.
+  ///
+  /// **Returns:**
+  /// A `Map` containing simplified paginated results.
+  ///
+  /// **Example Usage:**
+  /// Suppose you have a `QueryBuilder` configured for a database table:
+  /// ```dart
+  /// var queryBuilder = QueryBuilder();
+  /// var simplePaginationResult = await queryBuilder.simplePagination(10, 1);
+  /// print(simplePaginationResult);
+  /// ```
+  /// **Example Output:**
+  /// ```plaintext
+  /// {
+  ///   'next': 2,
+  ///   'previous': null,
+  ///   'last': 10,
+  ///   'total': 95,
+  ///   'data': [...]
+  /// }
+  /// ```
   @Deprecated(
       'Use simplePaginate instead. This method will be removed in future releases.')
   Future<Map> simplePagination([int perPage = 15, int page = 1]) async {
@@ -49,6 +112,32 @@ extension Pagination on QueryBuilder {
     ).response();
   }
 
+  /// Fetches data in a simple paginated format based on specified parameters.
+  ///
+  /// **Parameters:**
+  /// - [perPage]: Number of items per page, defaults to 15.
+  /// - [page]: Current page number, defaults to 1.
+  ///
+  /// **Returns:**
+  /// A `Map` containing simplified paginated results.
+  ///
+  /// **Example Usage:**
+  /// Example demonstrating calling `simplePaginate`:
+  /// ```dart
+  /// var queryBuilder = QueryBuilder();
+  /// var paginationResult = await queryBuilder.simplePaginate(10, 2);
+  /// print(paginationResult);
+  /// ```
+  /// **Example Output:**
+  /// ```plaintext
+  /// {
+  ///   'next': 3,
+  ///   'previous': 1,
+  ///   'last': 20,
+  ///   'total': 195,
+  ///   'data': [...]
+  /// }
+  /// ```
   Future<Map> simplePaginate([int perPage = 15, int page = 1]) async {
     final total = await count();
     final lastPage = (total / perPage).ceil();
